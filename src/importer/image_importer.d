cce19bd296e50135302166afb5acdc2d46e470ba
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-07 22:52:13 -0400

    Whittle down image_importer.d

--- src/importer/image_importer.d
+++ src/importer/image_importer.d
@@ -1,36 +1,16 @@
 module importer.image_importer;
 
+import image;
+
 /// Importer type for images.
 struct ImageImporter {
     /// Import an image from a path.
-    pub fn import_from_file(file: &Path) -> ImportResult<DynamicImage> {
-        <Self as Importer<&Path>>::import(file)
+    TrueColorImage import_from_file(string path) {
+        return loadImageFromFile(path).getAsTrueColorImage();
     }
 
     /// Import an image from memory.
-    pub fn import_from_memory(buf: &[u8]) -> ImportResult<DynamicImage> {
-        <Self as Importer<&[u8]>>::import(buf)
-    }
-}
-
-impl<'a> Importer<&'a Path> for ImageImporter {
-    type Texture = DynamicImage;
-
-    fn import(input: &Path) -> ImportResult<DynamicImage> {
-        match image::open(input) {
-            Ok(image) => Ok(image),
-            Err(e) => Err(format!("{}", e)),
-        }
-    }
-}
-
-impl<'a> Importer<&'a [u8]> for ImageImporter {
-    type Texture = DynamicImage;
-
-    fn import(input: &[u8]) -> ImportResult<DynamicImage> {
-        match image::load_from_memory(input) {
-            Ok(image) => Ok(image),
-            Err(e) => Err(format!("{}", e)),
-        }
+    TrueColorImage import_from_memory(ubyte[] buffer) {
+        loadImageFromMemory(cast(const(void)[]) buffer).getAsTrueColorImage();
     }
 }
