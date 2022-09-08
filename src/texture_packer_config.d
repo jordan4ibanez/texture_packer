eb63a810a54952e9b4d63be78599927eb8e0d59a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-07 22:37:43 -0400

    Translate texture_packer_config.d

--- src/texture_packer_config.d
+++ src/texture_packer_config.d
@@ -1,37 +1,38 @@
-use std::default::Default;
+module texture_packer_config;
+
 
 /// Configuration for a texture packer.
-#[derive(Debug, Copy, Clone)]
-pub struct TexturePackerConfig {
+struct TexturePackerConfig {
     //
     // layout configuration
     //
     /// Max width of the packed image. Default value is `1024`.
-    pub max_width: u32,
+    uint max_width = 1024;
     /// Max height of the packed image. Default value is `1024`.
-    pub max_height: u32,
+    uint max_height = 1024;
     /// True to allow rotation of the input images. Default value is `true`. Images rotated will be
     /// rotated 90 degrees clockwise.
-    pub allow_rotation: bool,
+    bool allow_rotation = true;
 
     //
     // texture configuration
     //
     /// Size of the padding on the outer edge of the packed image in pixel. Default value is `0`.
-    pub border_padding: u32,
+    uint border_padding = 0;
     /// Size of the padding between frames in pixel. Default value is `2`
-    pub texture_padding: u32,
+    uint texture_padding = 2;
     /// Size of the repeated pixels at the border of each image. Default value is `0`.
-    pub texture_extrusion: u32,
+    uint texture_extrusion = 0;
 
     /// True to trim the empty pixels of the input images. Default value is `true`.
-    pub trim: bool,
+    bool trim = true;
 
     /// True to draw the red line on the edge of the each frames. Useful for debugging. Default
     /// value is `false`.
-    pub texture_outlines: bool,
+    bool texture_outlines = false;
 }
 
+/*
 impl Default for TexturePackerConfig {
     fn default() -> TexturePackerConfig {
         TexturePackerConfig {
@@ -49,3 +50,4 @@ impl Default for TexturePackerConfig {
         }
     }
 }
+*/
