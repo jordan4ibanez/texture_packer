8faad59126541b7c13b1010c640b4696b204b79a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-08 21:15:15 -0400

    Begin completely re-engineering exporter

--- src/exporter/image_exporter.d
+++ src/exporter/image_exporter.d
@@ -1,44 +1,46 @@
 module exporter.image_exporter;
 
+import texture_packer;
+import image;
+
+/// Export a memory texture packer to a TrueTypeImage.
+TrueColorImage exportToImage(TexturePacker packer) {
+
+    uint width = packer.width();
+    uint height = packer.height();
+
+    assert(width == 0 || height == 0, "Width or height of this packer is zero");
 
 
-struct ImageExporter {
 
+    TrueColorImage newImage = TrueColorImage(width, height);
 
-    /// Export a memory texture packer to a TrueTypeImage.
-    fn export(texture: &T) -> ExportResult<DynamicImage> {
-        let width = texture.width();
-        let height = texture.height();
+    /*
 
-        if width == 0 || height == 0 {
-            return Err("Width or height of this texture is zero".to_string());
-        }
+    let mut pixels = Vec::with_capacity((width * height * 4) as usize);
 
-        let mut pixels = Vec::with_capacity((width * height * 4) as usize);
-
-        for row in 0..height {
-            for col in 0..width {
-                if let Some(pixel) = texture.get(col, row) {
-                    pixels.push(pixel[0]);
-                    pixels.push(pixel[1]);
-                    pixels.push(pixel[2]);
-                    pixels.push(pixel[3]);
-                } else {
-                    pixels.push(0);
-                    pixels.push(0);
-                    pixels.push(0);
-                    pixels.push(0);
-                }
+    for row in 0..height {
+        for col in 0..width {
+            if let Some(pixel) = texture.get(col, row) {
+                pixels.push(pixel[0]);
+                pixels.push(pixel[1]);
+                pixels.push(pixel[2]);
+                pixels.push(pixel[3]);
+            } else {
+                pixels.push(0);
+                pixels.push(0);
+                pixels.push(0);
+                pixels.push(0);
             }
-        }
-
-        if let Some(image_buffer) =
-            ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(width, height, pixels)
-        {
-            Ok(DynamicImage::ImageRgba8(image_buffer))
-        } else {
-            Err("Can't export texture".to_string())
         }
     }
 
+    if let Some(image_buffer) =
+        ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(width, height, pixels)
+    {
+        Ok(DynamicImage::ImageRgba8(image_buffer))
+    } else {
+        Err("Can't export texture".to_string())
+    }
+    */
 }
