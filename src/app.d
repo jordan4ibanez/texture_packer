482bec7f2209460697493bdcf0c2a3899c74afb3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-08 23:43:29 -0400

    Add more testing

--- src/app.d
+++ src/app.d
@@ -6,15 +6,27 @@ import texture_packer;
 import texture_packer_config;
 import image;
 import exporter.image_exporter;
+import std.conv: to;
+import arsd.png;
 
 
 void main() {
-    TexturePackerConfig config = TexturePackerConfig(400,400,true,0,0,0,true,false);
-    TexturePacker packer = TexturePacker(config);
 
-    TrueColorImage tempImageObject = loadImageFromFile("examples/assets/1.png").getAsTrueColorImage();
+    TexturePackerConfig config = TexturePackerConfig(400,400, false,2,0,0,true,true);
+    TexturePacker packer = TexturePacker.new_skyline(config);
 
-    packer.pack_own("blah", tempImageObject);
+
+    uint x = 1;
+    while (x <= 10){
+        TrueColorImage tempImageObject = loadImageFromFile("examples/assets/" ~ to!string(x) ~ ".png").getAsTrueColorImage();
+        packer.pack_own("blah" ~ to!string(x), tempImageObject);
+        x++;
+    }
+
+    writeln("exiting");
+    TrueColorImage myCoolPicture = exportToImage(packer);
+
+    writeImageToPngFile("test.png", myCoolPicture);
 
         
 }
